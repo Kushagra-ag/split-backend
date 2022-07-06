a95edadbc4504250fdadf1ada97592ac98268a95
kushagra Agarwal <kushagra.agarwal@razorpay.com>
2022-07-07 00:58:43 +0530

    init commit

--- functions/test.js
+++ functions/test.js
@@ -1,4 +1,4 @@
-import admin from 'firebase/admin';
+const admin = require('firebase-admin');
 
 // import * as admin from "firebase-admin";
 
@@ -13,7 +13,7 @@ admin.initializeApp({
   databaseURL: process.env.FIREBASE_DATABASE_URL
 });
 
-export const handler = async function (event, context) {
+exports.handler = async function (event, context) {
   const res = await admin.database().ref(`/users/HyjDA12c7bSAhUH_lV219`).once('value').then(res => res.val()).catch(e => `errrrrr ${e}`);
   console.log('logs from test func', event, context);
   return {
