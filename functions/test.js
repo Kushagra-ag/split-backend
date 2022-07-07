5b47c9954d0f361a146bfc5de0e138021979dfa6
kushagra Agarwal <kushagra.agarwal@razorpay.com>
2022-07-07 16:12:41 +0530

    firebase admin init

--- functions/test.js
+++ functions/test.js
@@ -1,17 +1,4 @@
-const admin = require('firebase-admin');
-
-// import * as admin from "firebase-admin";
-
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-// console.log(serviceAccount);
-
-// import serviceAccount from "../split-50cbf-firebase-adminsdk-1tev9-8d185587b6";
-// console.log(serviceAccount);
-
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: process.env.FIREBASE_DATABASE_URL
-});
+const admin = require('../firebase/admin');
 
 exports.handler = async function (event, context) {
   const res = await admin.database().ref(`/users/HyjDA12c7bSAhUH_lV219`).once('value').then(res => res.val()).catch(e => `errrrrr ${e}`);
